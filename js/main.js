e1f3771ecec74186323133079c8b815511457a7e
parvizx3 <129318294+parvizx3@users.noreply.github.com>
2025-02-09 16:57:11 +0330

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,17 +6,30 @@
     function determineLanguage(callback) {
       currentLang = localStorage.getItem("lang");
       if (currentLang) {
-        isRTL = ["fa"].includes(currentLang);
+        isRTL = currentLang === "fa";
         callback();
       } else {
-        // Get country code using ipinfo.io (replace YOUR_TOKEN with actual token)
-        $.get("https://ipinfo.io/json?token=YOUR_TOKEN", function (data) {
-          currentLang = data.country === "IR" ? "fa" : "en";
+        // Get country code using Cloudflare's trace endpoint
+        $.get("/cdn-cgi/trace", function (data) {
+          var traceInfo = {};
+          data.split('\n').forEach(function (line) {
+            var parts = line.split('=');
+            if (parts.length === 2) traceInfo[parts[0]] = parts[1];
+          });
+  
+          var countryCode = traceInfo.loc || 'US';
+          switch (countryCode) {
+            case 'IR': currentLang = 'fa'; break;  // Iran
+            case 'RU': currentLang = 'ru'; break;  // Russia
+            case 'CN': currentLang = 'zh'; break;  // China
+            default:   currentLang = 'en';         // All others
+          }
+  
           localStorage.setItem("lang", currentLang);
-          isRTL = currentLang === "fa";
+          isRTL = currentLang === 'fa';
           callback();
         }).fail(function () {
-          currentLang = "en";
+          currentLang = 'en';
           localStorage.setItem("lang", currentLang);
           isRTL = false;
           callback();
@@ -38,6 +51,10 @@
       });
   
       // Initialize plugins that depend on language direction
+      initializeCarousels();
+    }
+  
+    function initializeCarousels() {
       $(".screenshot-carousel").owlCarousel({
         autoplay: true,
         smartSpeed: 1000,
@@ -56,8 +73,8 @@
         nav: true,
         rtl: isRTL,
         navText: [
-          '<i class="bi bi-chevron-left"></i>',
-          '<i class="bi bi-chevron-right"></i>',
+          isRTL ? '<i class="bi bi-chevron-right"></i>' : '<i class="bi bi-chevron-left"></i>',
+          isRTL ? '<i class="bi bi-chevron-left"></i>' : '<i class="bi bi-chevron-right"></i>',
         ],
         responsive: {
           0: { items: 1 },
@@ -67,101 +84,13 @@
       });
     }
   
-    function updateContent(langData) {
-      $("[data-i18n]").each(function () {
-        var key = $(this).data("i18n");
-        $(this).text(langData[key] || $(this).text());
-      });
-    }
-  
-    function setupLanguageSwitcher(translations) {
-      $(".lang-select").on("click", function (e) {
-        e.preventDefault();
-        var lang = $(this).data("lang");
-        localStorage.setItem("lang", lang);
-        currentLang = lang;
-        isRTL = ["fa"].includes(lang);
-        
-        $("html")
-          .attr("dir", isRTL ? "rtl" : "ltr")
-          .attr("lang", lang)
-          .css("font-family", isRTL ? "Vazirmatn, sans-serif" : "inherit");
-        
-        $("body").toggleClass("rtl", isRTL);
-        updateContent(translations[lang]);
-        
-        // Reinitialize plugins
-        new WOW().init();
-        $('[data-toggle="counter-up"]').counterUp({
-          delay: 10,
-          time: 2000,
-        });
-        
-        // Reinitialize carousels with new direction
-        $(".screenshot-carousel, .testimonial-carousel")
-          .data("owl.carousel")
-          .destroy();
-        initializeApplication();
-      });
-    }
+    // ... (keep the rest of the functions identical to previous version - updateContent, setupLanguageSwitcher, and other initializations) ...
   
     // Start application
     determineLanguage(function () {
       initializeApplication();
     });
   
-    // Common initializations (language-independent)
-    var spinner = function () {
-      setTimeout(function () {
-        if ($("#spinner").length > 0) {
-          $("#spinner").removeClass("show");
-        }
-      }, 1);
-    };
-    spinner();
+    // ... (remaining code identical to previous version) ...
   
-    new WOW().init();
-  
-    $(window).scroll(function () {
-      if ($(this).scrollTop() > 45) {
-        $(".navbar").addClass("sticky-top shadow-sm");
-      } else {
-        $(".navbar").removeClass("sticky-top shadow-sm");
-      }
-    });
-  
-    $(".navbar-nav a").on("click", function (event) {
-      if (this.hash !== "") {
-        event.preventDefault();
-        $("html, body").animate(
-          {
-            scrollTop: $(this.hash).offset().top - 45,
-          },
-          1500,
-          "easeInOutExpo"
-        );
-        if ($(this).parents(".navbar-nav").length) {
-          $(".navbar-nav .active").removeClass("active");
-          $(this).closest("a").addClass("active");
-        }
-      }
-    });
-  
-    $(window).scroll(function () {
-      if ($(this).scrollTop() > 100) {
-        $(".back-to-top").fadeIn("slow");
-      } else {
-        $(".back-to-top").fadeOut("slow");
-      }
-    });
-  
-    $(".back-to-top").click(function () {
-      $("html, body").animate({ scrollTop: 0 }, 1500, "easeInOutExpo");
-      return false;
-    });
-  
-    $('[data-toggle="counter-up"]').counterUp({
-      delay: 10,
-      time: 2000,
-    });
   })(jQuery);
